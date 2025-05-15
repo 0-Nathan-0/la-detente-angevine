18f6c04010ab14ae396ae32f3d6615ae42755bdf
0-Nathan-0 <147658261+0-Nathan-0@users.noreply.github.com>
2025-05-16 01:07:13 +0200

    lil fix

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,11 +3,8 @@ import React from "react";
 const Hero: React.FC = () => {
   return (
     <section
-      className="relative h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url('/chemin/vers/ton-image.jpg')" }}
     >
       <img
-        id="photoDeCouverture"
         src="https://plus.unsplash.com/premium_photo-1670984940156-c7f833fe8397?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFyfGVufDB8fDB8fHww"
         alt="Bar"
         className="w-full h-screen object-cover"
